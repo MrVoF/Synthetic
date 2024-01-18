288063730d409ecd2d7dc9f01df7c4def27bf339
vvtyu <vvtyurmin@gmail.com>
2024-01-18 18:38:11 +0300

    Фикс багов в заполнении синтетики. Новые синтетические данные.

--- promon_faker.py
+++ promon_faker.py
@@ -11,14 +11,19 @@ class Fake:
         stype = stype.upper()
         if stype == 'UUID': return self.uuid()
         if stype == 'TEXT': return self.text()
-        if stype == 'STRIM': return self.strim()
-        if stype == 'TEAM': return self.team()
         if stype == 'INT': return self.int()
         if stype == 'FLOAT': return self.float()
         if stype == 'BOOLEAN': return self.bool()
+        if stype == 'PERCENT': return self.percent()
         if stype == 'DATE': return self.date()
-        if stype == 'TIMESTAMP': return self.timestamp()
+        if stype == 'DATETIME': return self.datetime()
         if stype == 'NUMERIC': return self.numeric()
+        if stype == 'STRIM': return self.strim()
+        if stype == 'TEAM': return self.team()
+        if stype == 'TEAM': return self.team()
+        if stype == 'TEAM': return self.team()
+        if stype == 'NAME': return self.name()
+
         return ''
 
     def __str__(self):
@@ -32,14 +37,6 @@ class Fake:
     def text(self):
         '''Генерация случайного текста'''
         return "'" + self.faker.text().replace("'", '`') + "'"
-
-    def strim(self):
-        '''Генерация случайных названий стримов'''
-        return "'" + self.faker.company().replace("'", '`') + "'"
-
-    def team(self):
-        '''Генерация случайных названий команд'''
-        return "'" + self.faker.bs().replace("'", '`') + "'"
 
     def int(self):
         '''Генерация случайных целых чисел'''
@@ -57,10 +54,26 @@ class Fake:
         '''Генерация случайных булевых значений'''
         return self.faker.pybool()
 
+    def percent(self):
+        '''Генерация случайных процентов'''
+        return self.faker.pyfloat(left_digits=2, right_digits=2, min_value=0, max_value=1)
+
     def date(self):
         '''Генерация случайных дат'''
-        return self.faker.date()
+        return "'" + self.faker.date() + "'"
 
-    def timestamp(self):
+    def datetime(self):
         '''Генерация случайных дат с временем'''
-        return self.faker.date_time()
+        return "'" + str(self.faker.date_time()) + "'"
+
+    def strim(self):
+        '''Генерация случайных названий стримов'''
+        return "'" + self.faker.company().replace("'", '`') + "'"
+
+    def team(self):
+        '''Генерация случайных названий команд'''
+        return "'" + self.faker.bs().replace("'", '`') + "'"
+
+    def name(self):
+        '''Генерация случайных имен'''
+        return "'" + self.faker.name().replace("'", '`') + "'"
