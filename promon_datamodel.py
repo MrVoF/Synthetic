1d07ee482b62192ca8c83ab5568807effd010f59
vvtyu <vvtyurmin@gmail.com>
2024-01-26 14:47:41 +0300

    Добавил фейковые типы Спринт, Команда и Стрим.

--- promon_datamodel.py
+++ promon_datamodel.py
@@ -1,10 +1,12 @@
 from promon_postgre import PSQLConnect
 from promon_faker import Fake
 from promon_yaml import Yaml
+import random
 
 
 def synthetic_data(file_name='data.yml'):
     yaml = Yaml(file_name=file_name)
+    fake = Fake()
     host = yaml.get_data()['host']
     database = yaml.get_data()['database']
     schema = yaml.get_data()['schema']
@@ -17,15 +19,14 @@ def synthetic_data(file_name='data.yml'):
         for table in yaml.get_data()['tables']:
             psql.drop_table(table['name'])
             psql.create_table(table['name'], table['columns'])
-
             if yaml.get_data()['datacount'] > 0:
-                fake = Fake()
                 for _ in range(yaml.get_data()['datacount']):
                     columns = ''
                     values = ''
+                    rnd = random.randint(0, 9)
                     for column in table['columns']:
                         columns += '"' + column['name'] + '", '
-                        values += str(fake.get_data(column['stype'])) + ', '
+                        values += str(fake.get_data(rnd, column['stype'])) + ', '
                     psql.insert_table(table['name'], columns[:-2], values[:-2])
 
         psql.close_connection()
--- promon_faker.py
+++ promon_faker.py
@@ -6,8 +6,16 @@ class Fake:
     def __init__(self, locale='ru_RU'):
         self.locale = locale
         self.fake = Faker(locale)
+        self.teams = []
+        self.strims = []
 
-    def get_data(self, stype='text'):
+        for _ in range(100):
+            self.teams += [(self.uuid(), self.text(50))]
+
+        for _ in range(100):
+            self.strims += [(self.uuid(), self.text(50))]
+
+    def get_data(self, rnd=0, stype='text'):
         import re
 
         stype = stype.lower()
@@ -23,6 +31,13 @@ class Fake:
         if stype == 'datetime': return self.datetime()
         if stype == 'numeric': return self.numeric()
         if stype == 'name': return self.name()
+        if stype == 'supersprint': return self.supersprint()
+        if stype == 'sprint': return self.sprint()
+        if stype == 'teamid': return self.team_id(rnd)
+        if stype == 'team': return self.team(rnd)
+        if stype == 'strimid': return self.strim_id(rnd)
+        if stype == 'strim': return self.strim(rnd)
+
         return ''
 
     def __str__(self):
@@ -67,3 +82,42 @@ class Fake:
     def name(self):
         """Генерация случайных имен"""
         return "'" + self.fake.name().replace("'", '`') + "'"
+
+    def supersprint(self):
+        """Генерация случайных суперспринтов"""
+        import random
+        from datetime import datetime
+
+        supersprint = (str(datetime.now().year) + '.' +
+                       str(random.choice(['0.1', '0.2',
+                                          '1.1', '1.2', '1.3', '1.4', '1.5', '1.6',
+                                          '2.1', '2.2', '2.3', '2.4', '2.5', '2.6',
+                                          '3.1', '3.2', '3.3', '3.4', '3.5', '3.6',
+                                          '4.1', '4.2', '4.3', '4.4', '4.5', '4.6'])
+                           )
+                       )
+        return "'" + supersprint + "'"
+
+    def sprint(self):
+        """Генерация случайных спринтов"""
+        import random
+        from datetime import datetime
+
+        sprint = str(datetime.now().year) + '.' + str(random.randint(0, 4))
+        return "'" + sprint + "'"
+
+    def team_id(self, rnd):
+        """Генерация случайного id команды"""
+        return self.teams[rnd][0]
+
+    def strim_id(self, rnd):
+        """Генерация случайного id стрима"""
+        return self.strims[rnd][0]
+
+    def team(self, rnd):
+        """Генерация случайной команды"""
+        return self.teams[rnd][1]
+
+    def strim(self, rnd):
+        """Генерация случайного стрима"""
+        return self.strims[rnd][1]
