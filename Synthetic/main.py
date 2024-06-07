ee2afadb7fceae155a560bb61d1d29965fd0e1e2
Vladimir Tyurmin <vvtyurmin@gmail.com>
2024-06-07 14:23:55 +0300

    Delete Synthetic directory

--- Synthetic/main.py
+++
@@ -1,8 +0,0 @@
-from promon_datamodel import synthetic_data
-from promon_datamodel import json_data
-
-# synthetic_data("Python\\Synthetic\\yml\\data.yml")
-# synthetic_data("Python\\Synthetic\\yml\\planing.yml")
-synthetic_data("Python\\Synthetic\\yml\\strim_standarts.yml")
-# synthetic_data("Python\\Synthetic\\yml\\test.yml")
-# json_data("Python\\Synthetic\\yml\\json_test.yml")
